e85b9ed4fd13ccd08e391ad07650ae709d4fb3ea
Sunny Nian <sunny.nian1@gmail.com>
2018-11-24 16:33:27 -0700

    more stuff

--- WordCount.py
+++ WordCount.py
@@ -1,10 +1,43 @@
-
-with open("example.txt","r") as a:
+with open("example.txt", "r") as a:
     b = a.read()
+    b = b.replace("\n", ' ')
+    b = b.replace("—", ' ')
+    b = b.replace("-", ' ')
+    b = b.replace(".", ' ')
 
-allowed = set("abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ'")
-item = ''.join(filter(allowed.__contains__, b))
+def alphabetical():
+    count = {}
+    c = b
+    allowed = set("abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ'")
+    c = ''.join(filter(allowed.__contains__, c))
+    c = c.split()
 
-c = item.split()
+    for i in c:
+        i = i.lower()
+        if i in count:
+            count[i] += 1
+        else:
+            count[i] = 1
 
-for i in
+    for key in sorted(count):
+        print("%s: %s" % (key, count[key]))
+
+
+def number():
+    count = {}
+    c = b
+    allowed = set("abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ'")
+    c = ''.join(filter(allowed.__contains__, c))
+    c = c.split()
+
+    for i in c:
+        i = i.lower()
+        if i in count:
+            count[i] += 1
+        else:
+            count[i] = 1
+
+    sortedCount = dict(sorted(count.items(), key=lambda kv: kv[1])[::-1])
+
+    for key in sortedCount:
+        print("{}: {}".format(key, sortedCount[key]))
